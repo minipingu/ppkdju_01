10c918732fab2d14d2b2b6b54793fcc4a3b01ac2
Dede nurhidayat <ranmaniax@gmail.com>
2026-09-16 14:34:00 +0700

    persiapan membuat splash screen

---
+++ android/app/src/main/kotlin/com/example/ppkdju_01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ppkdju_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreenDay15 extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<SplashScreenDay15> createState() => _SplashScreenDay15State();
+}
+
+class _SplashScreenDay15State extends State<SplashScreenDay15> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
